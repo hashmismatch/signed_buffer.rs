e10607f75a6516a6970de4e07ff723c4bfe04c41
Rudi Benkovic <rudi.benkovic@gmail.com>
2015-04-15 01:02:16 +0200

    int to byte fail

--- src/lib.rs
+++ src/lib.rs
@@ -59,17 +59,31 @@ impl<'a> SignedBuffer<'a> {
 			buffer[buffer.len() - self.trailer_magic_bytes.len() + i] = self.trailer_magic_bytes[i];
 		}
 
-		let checksum = SignedBuffer::hash(buffer);
+		let checksum = self.hash(buffer);
 		let checksum_start_addr = buffer.len() - self.trailer_magic_bytes.len() - self.hash_size_bytes() as usize;
 		
+
+		{
+			buffer[checksum_start_addr + 0] = ((checksum & (0xff00000000000000)) >> 7) as u8;
+			buffer[checksum_start_addr + 1] = ((checksum & (0x00ff000000000000)) >> 6) as u8;
+			buffer[checksum_start_addr + 2] = ((checksum & (0x0000ff0000000000)) >> 5) as u8;
+			buffer[checksum_start_addr + 3] = ((checksum & (0x000000ff00000000)) >> 4) as u8;
+			buffer[checksum_start_addr + 4] = ((checksum & (0x00000000ff000000)) >> 3) as u8;
+			buffer[checksum_start_addr + 5] = ((checksum & (0x0000000000ff0000)) >> 2) as u8;
+			buffer[checksum_start_addr + 6] = ((checksum & (0x000000000000ff00)) >> 1) as u8;
+			buffer[checksum_start_addr + 7] = ((checksum & (0x00000000000000ff)) >> 0) as u8;
+		}
+
+		/*
 		for i in 0..self.hash_size_bytes() as usize {
 			let r = checksum.rotate_right((self.hash_size_bytes() as usize - i) as u32);
 			buffer[checksum_start_addr + i as usize] = (r & 0xff) as u8;
+			//buffer]
 		}
+		*/
 
-		for i in 0..2 {
-			buffer[i + self.header_magic_bytes.len()] = (payload.len() >> (1 - i as u32) & 0xff) as u8;
-		}
+		buffer[self.header_magic_bytes.len() + 0] = ((payload.len() & 0xff00 << 0) >> 1) as u8;
+		buffer[self.header_magic_bytes.len() + 1] = ((payload.len() & 0xff << 0) >> 0) as u8;
 
 		for i in 0..payload.len() {
 			buffer[2 + self.header_magic_bytes.len() + i] = payload[i];
@@ -98,21 +112,46 @@ impl<'a> SignedBuffer<'a> {
 
 		let payload_bytes = {
 			let mut p: u16 = 0;
-			p += (buffer[self.header_magic_bytes.len() + 0] << 1) as u16;
-			p += (buffer[self.header_magic_bytes.len() + 1] << 0) as u16;
+			p += ((buffer[self.header_magic_bytes.len() + 0] as u16) << 1);
+			p += ((buffer[self.header_magic_bytes.len() + 1] as u16) << 0);
 
 			p
 		};
 
 
 		let s = self.header_magic_bytes.len() + 2;
-
-		return Ok(Payload {
+		let payload = Payload {
 			payload: &buffer[s..(s + payload_bytes as usize)]
-		});
+		};
+
+		let checksum_in_payload = {
+			let mut c: u64 = 0;
+			let mut s = buffer.len() - self.trailer_magic_bytes.len() - self.hash_size_bytes() as usize;
+			
+			c += (buffer[s + 0] as u64) << 7;
+			c += (buffer[s + 1] as u64) << 6;
+			c += (buffer[s + 2] as u64) << 5;
+			c += (buffer[s + 3] as u64) << 4;
+			c += (buffer[s + 4] as u64) << 3;
+			c += (buffer[s + 5] as u64) << 2;
+			c += (buffer[s + 6] as u64) << 1;
+			c += (buffer[s + 7] as u64) << 0;
+
+			c
+		};
+
+		let checksum = self.hash(payload.payload);
+		
+		if checksum_in_payload != checksum {
+			return Err(PayloadRetrievalError::InvalidHash);
+		}
+		
+
+
+		return Ok(payload);
 	}
 
-	fn hash(buffer: &[u8]) -> u64 {
+	fn hash(&self, buffer: &[u8]) -> u64 {
 		let mut hash = SipHasher::new();
 
 		for b in buffer {
